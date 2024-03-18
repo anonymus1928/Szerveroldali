74dbd45fdf8de7bb48a0bf4266d4b093f8a1e551
anonymus1928 <anonymus1928@gmail.com>
2024-03-18 21:57:19 +0100

    4. gyakorlat | Blade, Controller, Route

---
+++ kedd/ticket/app/Http/Controllers/TicketController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tickets = Ticket::where('done', 0)->get();
+        return view('ticket.tickets', ['tickets' => $tickets, 'closed' => false]);
+    }
+
+    /**
+     * Display a listing of the closed resource.
+     */
+    public function indexClosed()
+    {
+        $tickets = Ticket::where('done', 1)->get();
+        return view('ticket.tickets', ['tickets' => $tickets, 'closed' => true]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
