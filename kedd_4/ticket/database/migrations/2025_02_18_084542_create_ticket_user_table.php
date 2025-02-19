e0b3878f55c389c0fbb54338fda3db301c3d5349
Németh Tamás <anonymus1928@gmail.com>
2025-02-19 09:18:36 +0100

    2. gyakorlat | Migration, Model

---
+++ kedd_4/ticket/database/migrations/2025_02_18_084542_create_ticket_user_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ticket_user', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('owner')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ticket_user');
+    }
+};
---
+++ kedd_8/ticket/app/Models/Ticket.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    /** @use HasFactory<\Database\Factories\TicketFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'title',
+        'done',
+        'priority',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'admin' => 'boolean',
+        ];
+    }
+}
