b4f0451dca2fd2532c070c13cb787fc5cc983b9a
Németh Tamás <anonymus1928@gmail.com>
2025-09-17 11:15:33 +0200

    2. gyakorlat | Migration, Model, Adatbázistervezés

---
+++ kedd/ticket/app/Models/Comment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /** @use HasFactory<\Database\Factories\CommentFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'description',
+        'filename',
+        'filename_hash',
+    ];
+}
--- kedd/ticket/app/Models/User.php
+++ kedd/ticket/app/Models/User.php
@@ -43,6 +43,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'admin' => 'boolean',
         ];
     }
 }
