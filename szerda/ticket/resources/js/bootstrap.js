bbc7eac41dfe3922ddb041fd7cad3429c16f41b0
Németh Tamás <anonymus1928@gmail.com>
2026-03-18 14:03:10 +0100

    5. gyakorlat | View, Vite, Controller

--- szerda/ticket/resources/js/bootstrap.js
+++ szerda/ticket/resources/js/bootstrap.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import 'bootstrap';
+
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
---
+++ szerda/ticket/resources/views/ticket/tickets.blade.php
@@ -0,0 +1,73 @@
+@extends('ticket.layout')
+
+@section('title', 'Feladatok')
+
+@section('content')
+    <h1 class="ps-3">Feladatok</h1>
+    <hr />
+    <div class="table-responsive">
+        <table class="table align-middle table-hover">
+            <thead class="text-center table-light">
+                <tr>
+                    <th style="width: 10%">Priorítás</th>
+                    <th style="width: 15%">Beküldő</th>
+                    <th style="width: 15%">Utolsó hozzászóló</th>
+                    <th style="width: 40%">Tárgy</th>
+                    <th style="width: 10%">Státusz</th>
+                    <th style="width: 10%"></th>
+                </tr>
+            </thead>
+            <tbody class="text-center">
+
+                @foreach($tickets as $ticket)
+                    <tr class="@if($ticket->priority == 3) table-danger @elseif($ticket->priority == 2) table-warning @endif">
+                        <td>
+                            @switch($ticket->priority)
+                                @case(0)
+                                    <span class="badge rounded-pill bg-info fs-6">Alacsony</span>
+                                    @break
+                                @case(1)
+                                    <span class="badge rounded-pill bg-success fs-6">Normál</span>
+                                    @break
+                                @case(2)
+                                    <span class="badge rounded-pill bg-warning fs-6">Magas</span>
+                                    @break
+                                @case(3)
+                                    <span class="badge rounded-pill bg-danger text-white fs-6">Azonnal</span>
+                                    @break
+                            @endswitch
+                        </td>
+                        <td>
+                            <div>Teszt Felhasználó</div>
+                            <div class="text-secondary">2022. 02. 11. 10:48</div>
+                        </td>
+                        <td>
+                            <div>{{ $ticket->comments->sortByDesc('created_at')->first()->user->name }}</div>
+                            <div class="text-secondary">{{ $ticket->comments->sortByDesc('created_at')->first()->created_at }}</div>
+                        </td>
+                        <td>
+                            <div>
+                                <a href="feladat.html">{{ $ticket->title }}</a>
+                            </div>
+                        </td>
+                        <td>
+                            @if ($ticket->done)
+                                <span class="badge rounded-pill bg-info text-dark fs-6">Lezárva</span>
+                            @elseif ($ticket->comments->count() === 1)
+                                <span class="badge rounded-pill bg-info text-dark fs-6">Új</span>
+                            @else
+                                <span class="badge rounded-pill bg-info text-dark fs-6">Folyamatban</span>
+                            @endif
+                        </td>
+                        <td>
+                            <button class="btn btn-outline-secondary">
+                                <i class="fa-solid fa-angles-right fa-fw"></i>
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
+@endsection
